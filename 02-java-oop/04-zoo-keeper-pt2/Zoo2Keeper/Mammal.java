a50a6186dcf47f7d44994203624fc54e081cf2ac
Nathan Mueller <mueller.nathanc24@gmail.com>
2021-08-14 16:18:40 -0400

    Updated ZooKeep2

--- 02-java-oop/04-zoo-keeper-pt2/Zoo2Keeper/Mammal.java
+++ 02-java-oop/04-zoo-keeper-pt2/Zoo2Keeper/Mammal.java
@@ -1,6 +1,6 @@
 public class Mammal {
     protected int energyLevel;  //protected modifier because Gorilla inherits attributes of Mammal
-                                //protected modifier attributed visible on all child classes ie Gorilla
+                                //protected modifier attributed visible on all child classes ie GiantBat
     public Mammal(int energyLevel) {
         this.energyLevel = energyLevel;
     }
